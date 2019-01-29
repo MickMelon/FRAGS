6db3a20ecda5a5259f93201f9612f3a70b8d7d3a
MickMelon <michael.mcmillan@outlook.com>
2019-01-29 00:12:21 +0000

    Initial commit

---
+++ src/Frags.Discord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frags.Discord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
